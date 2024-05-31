ce7d48a5c7fcda0dc4924072651a7901f059d1ea
jinsol <emcis44@gmail.com>
2024-05-31 15:35:14 +0900

    진솔_24.05.31_15:35

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -2,181 +2,170 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import HeaderNav from "./HeaderNav";
 import { useLocation, useNavigate } from "react-router-dom";
-import {
-  FaAlignJustify,
-  FaSistrix,
-  FaFilter,
-  FaAngleLeft,
-} from "react-icons/fa6";
+import { FaAlignJustify, FaSistrix, FaFilter, FaAngleLeft } from "react-icons/fa6";
 
 const HeaderBlock = styled.header`
-  background-color: white;
-  position: sticky;
-  top: 0;
-  z-index: 3;
+	background-color: white;
+	position: sticky;
+	top: 0;
+	z-index: 3;
 `;
 
 const HeaderMain = styled.ul`
-  padding: 16px 0;
-  li {
-    width: 30%;
-    font-size: 16px;
-    color: var(--black-color);
-    button svg {
-      font-size: 20px;
-    }
-    &.TA_Right {
-      display: flex;
-      gap: 8px;
-      justify-content: flex-end;
-      .done {
-        color: var(--main-color);
-        font-family: var(--default-font);
-        font-weight: 700;
-      }
-    }
-  }
+	padding: 16px 0;
+	li {
+		width: 30%;
+		font-size: 16px;
+		color: var(--black-color);
+		button svg {
+			font-size: 20px;
+		}
+		&.TA_Right {
+			display: flex;
+			gap: 8px;
+			justify-content: flex-end;
+			.done {
+				color: var(--main-color);
+				font-family: var(--default-font);
+				font-weight: 700;
+			}
+		}
+	}
 `;
 
 const HeaderBack = styled.div`
-  background-color: rgba(0, 0, 0, 0.7);
-  opacity: 0;
-  visibility: hidden;
-  position: fixed;
-  inset: 0;
-  z-index: 1;
-  transition: all 0.3s;
-  &.active {
-    opacity: 1;
-    visibility: visible;
-  }
+	background-color: rgba(0, 0, 0, 0.7);
+	opacity: 0;
+	visibility: hidden;
+	position: fixed;
+	inset: 0;
+	z-index: 1;
+	transition: all 0.3s;
+	&.active {
+		opacity: 1;
+		visibility: visible;
+	}
 `;
 
 const Header = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const [headerTitle, setHeaderTitle] = useState("해비티카");
-  const [sidebarActive, setSidebarActive] = useState(false);
+	const location = useLocation();
+	const navigate = useNavigate();
+	const [headerTitle, setHeaderTitle] = useState("해비티카");
+	const [sidebarActive, setSidebarActive] = useState(false);
 
-  const onClickSidebar = () => setSidebarActive(true);
-  const onClickBack = () => setSidebarActive(false);
+	const onClickSidebar = () => setSidebarActive(true);
+	const onClickBack = () => setSidebarActive(false);
 
-  useEffect(() => {
-    const findTitle = () => {
-      if (location.pathname === "/") return "해비티카";
+	useEffect(() => {
+		const findTitle = () => {
+			if (location.pathname === "/") return "해비티카";
 
-      for (let menu of headerMenu) {
-        for (let item of menu.list) {
-          if (location.pathname === item.listLink) return item.listTitle;
-        }
-      }
+			for (let menu of headerMenu) {
+				for (let item of menu.list) {
+					if (location.pathname === item.listLink) return item.listTitle;
+				}
+			}
 
-      switch (location.pathname) {
-        case "/setting/settings":
-          return "설정";
-        case "/setting/message":
-          return "메시지";
-        case "/character/profile":
-          return "내 프로필";
-        default:
-          return "해비티카";
-      }
-    };
+			switch (location.pathname) {
+				case "/setting/settings":
+					return "설정";
+				case "/setting/message":
+					return "메시지";
+				case "/character/profile":
+					return "내 프로필";
+				default:
+					return "해비티카";
+			}
+		};
 
-    setHeaderTitle(findTitle());
-    setSidebarActive(false);
-  }, [location]);
+		setHeaderTitle(findTitle());
+		setSidebarActive(false);
+	}, [location]);
 
-  const onClickPrev = () => navigate(-1);
-  const onClickDone = () => navigate("/");
+	const onClickPrev = () => navigate(-1);
+	const onClickDone = () => navigate("/");
 
-  const headerMenu = [
-    {
-      title: "캐릭터",
-      icon: "",
-      list: [
-        { listTitle: "스킬", listLink: "/character/skills" },
-        { listTitle: "스탯", listLink: "/character/stats" },
-        { listTitle: "도전과제", listLink: "/character/achievements" },
-      ],
-    },
-    {
-      title: "상점",
-      list: [{ listTitle: "상점", listLink: "/shop/market" }],
-    },
-    {
-      title: "인벤토리",
-      list: [
-        { listTitle: "장비", listLink: "/inventory/equipment" },
-        { listTitle: "아이템", listLink: "/inventory/items" },
-      ],
-    },
-    {
-      title: "소셜",
-      list: [
-        { listTitle: "지뢰찾기", listLink: "/app" },
-       
-      ],
-    },
-    {
-      title: "소개",
-      list: [
-        { listTitle: "소개", listLink: "/about/company" },
-        { listTitle: "뉴스", listLink: "/about/news" },
-        { listTitle: "게시판", listLink: "/about/board" },
-      ],
-    },
-  ];
+	const headerMenu = [
+		{
+			title: "캐릭터",
+			icon: "",
+			list: [
+				{ listTitle: "스킬", listLink: "/character/skills" },
+				{ listTitle: "스탯", listLink: "/character/stats" },
+				{ listTitle: "도전과제", listLink: "/character/achievements" },
+			],
+		},
+		{
+			title: "상점",
+			list: [{ listTitle: "상점", listLink: "/shop/market" }],
+		},
+		{
+			title: "인벤토리",
+			list: [
+				{ listTitle: "장비", listLink: "/inventory/equipment" },
+				{ listTitle: "아이템", listLink: "/inventory/items" },
+			],
+		},
+		{
+			title: "소셜",
+			list: [{ listTitle: "지뢰찾기", listLink: "/app" }],
+		},
+		{
+			title: "소개",
+			list: [
+				{ listTitle: "소개", listLink: "/about/company" },
+				{ listTitle: "뉴스", listLink: "/about/news" },
+				{ listTitle: "게시판", listLink: "/about/board" },
+			],
+		},
+	];
 
-  return (
-    <HeaderBlock>
-      <HeaderMain className="HeaderMain DefaultWidth FL_SB">
-        <li className="TA_Left">
-          {location.pathname === "/" ? (
-            <button type="button" onClick={onClickSidebar}>
-              <FaAlignJustify />
-              <span className="blind">사이드바</span>
-            </button>
-          ) : (
-            !location.pathname.startsWith("/setting") && (
-              <button type="button" onClick={onClickPrev}>
-                <FaAngleLeft />
-                <span className="blind">이전</span>
-              </button>
-            )
-          )}
-        </li>
-        <li className="TA_Center">
-          <h1 className="FontTitle">{headerTitle}</h1>
-        </li>
-        <li className="right_section TA_Right">
-          {location.pathname === "/" ? (
-            <>
-              <button type="button">
-                <FaSistrix />
-                <span className="blind">검색</span>
-              </button>
-              <button type="button">
-                <FaFilter />
-                <span className="blind">필터</span>
-              </button>
-            </>
-          ) : (
-            location.pathname.startsWith("/setting") && (
-              <button type="button" className="done" onClick={onClickDone}>
-                완료
-              </button>
-            )
-          )}
-        </li>
-      </HeaderMain>
-      <HeaderNav headerMenu={headerMenu} sidebarActive={sidebarActive} />
-      <HeaderBack
-        className={`HeaderBack ${sidebarActive ? "active" : ""}`}
-        onClick={onClickBack}
-      ></HeaderBack>
-    </HeaderBlock>
-  );
+	return (
+		<HeaderBlock>
+			<HeaderMain className="HeaderMain DefaultWidth FL_SB">
+				<li className="TA_Left">
+					{location.pathname === "/" ? (
+						<button type="button" onClick={onClickSidebar}>
+							<FaAlignJustify />
+							<span className="blind">사이드바</span>
+						</button>
+					) : (
+						!location.pathname.startsWith("/setting") && (
+							<button type="button" onClick={onClickPrev}>
+								<FaAngleLeft />
+								<span className="blind">이전</span>
+							</button>
+						)
+					)}
+				</li>
+				<li className="TA_Center">
+					<h1 className="FontTitle">{headerTitle}</h1>
+				</li>
+				<li className="right_section TA_Right">
+					{location.pathname === "/" ? (
+						<>
+							<button type="button">
+								<FaSistrix />
+								<span className="blind">검색</span>
+							</button>
+							<button type="button">
+								<FaFilter />
+								<span className="blind">필터</span>
+							</button>
+						</>
+					) : (
+						location.pathname.startsWith("/setting") && (
+							<button type="button" className="done" onClick={onClickDone}>
+								완료
+							</button>
+						)
+					)}
+				</li>
+			</HeaderMain>
+			<HeaderNav headerMenu={headerMenu} sidebarActive={sidebarActive} />
+			<HeaderBack className={`HeaderBack ${sidebarActive && "active"}`} onClick={onClickBack}></HeaderBack>
+		</HeaderBlock>
+	);
 };
 
 export default Header;
--- client/src/components/reward/RewardYourSelf.jsx
+++ client/src/components/reward/RewardYourSelf.jsx
@@ -1,14 +1,51 @@
 import React from "react";
+import styled from "styled-components";
+
+const RewardYourSelfBlock = styled.section`
+	background-color: whitesmoke;
+
+	margin-bottom: 24px;
+	border-radius: 8px;
+	overflow: hidden;
+	li {
+		&.TA_Left {
+			padding: 16px;
+		}
+		&.TA_Right {
+			background-color: var(--gray-hover);
+			flex-basis: 10%;
+			button {
+				padding: 16px;
+				gap: 8px;
+				box-sizing: border-box;
+				width: 100%;
+				.icon {
+					width: 20px;
+				}
+			}
+		}
+	}
+`;
 
 const RewardYourSelf = () => {
-  return (
-    <section>
-      <ul>
-        <li className="TA_Left">dd</li>
-        <li className="TA_Right">dd</li>
-      </ul>
-    </section>
-  );
+	return (
+		<RewardYourSelfBlock>
+			<ul className="FL_SB FL_Center">
+				<li className="TA_Left">
+					<h3 className="title FontMenuTitle">스스로에게 보상을 주세요!</h3>
+					<p className="content FontSub">TV를 보거나, 게임을 하거나, 간식을 먹거나, 마음대로 하세요!</p>
+				</li>
+				<li className="TA_Right">
+					<button type="button" className="FL_Column FL_Center">
+						<span className="icon">
+							<img src="../src/assets/image/coin.svg" alt="코인" />
+						</span>
+						<span className="FontMenuTitle">20</span>
+					</button>
+				</li>
+			</ul>
+		</RewardYourSelfBlock>
+	);
 };
 
 export default RewardYourSelf;
